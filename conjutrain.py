34d69511cb31e8f3e0bf2f28c494020ba4de4288
Friedrich Eibl <65565222+friedrich-eibl@users.noreply.github.com>
2024-11-27 22:45:51 +0000

    ADD progress saving

--- conjutrain.py
+++ conjutrain.py
@@ -2,35 +2,62 @@ import csv
 import random
 
 dict_file = "test_dict.csv"
-data = []
-with open(dict_file, mode='r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        data.append(row)
+eval_file = "eval.csv"
 
 
+def write_csv(data, f_csv):
+    with open(f_csv, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(data)
 
-#print(data)
+def train():
+    data = []
+    with open(dict_file, mode='r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            data.append(row)
 
-rounds = 0
-max_rounds = 30
-score = 0
+    learned = []
+    with open(eval_file, mode='r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            learned.append(row)
 
-pronouns = ["я", "ты", "он", "мы", "вы", "они"]
-while rounds < max_rounds:
-    rounds+=1
+    #print(data)
 
-    word = random.choice(data)
-    form = random.randint(1, 6)
+    rounds = 0
+    max_rounds = 30
+    score = 0
 
-    sub = input(f"{word[0]}, {form}:        {pronouns[form-1]} ")
+    pronouns = ["я", "ты", "он", "мы", "вы", "они"]
+    while rounds < max_rounds:
+        if not any('0' in word_learned for word_learned in learned):
+            print('nothing left to learn !!!')
+            return
+        
+        rounds+=1
 
-    if sub == word[form].strip():
-        score+=1
-        print('correct\n')
-    else:
-        print('nope\n')
-        #print(word[form]) ## debug
+        word_idx = random.randint(0, len(data) - 1)
+        word = data[word_idx]
+        form = random.randint(1, 6)
+
+        if learned[word_idx][form] == 1:
+            rounds -= 1
+            continue
+
+        sub = input(f"{word[0]}, {form}:        {pronouns[form-1]} ")
+
+        if sub == word[form].strip():
+            score+=1
+            learned[word_idx][form] = 1
+            write_csv(learned, eval_file)
+            print('correct\n')
+        else:
+            print('nope\n')
+            #print(word[form]) ## debug
 
 
-print(f"Score:  {score}/{max_rounds}")
+    print(f"Score:  {score}/{max_rounds}")
+
+
+train()
