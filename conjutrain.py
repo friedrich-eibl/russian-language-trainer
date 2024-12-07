17ecb2f77d0807dfe7ceb0e7f227cab450fe445a
Friedrich Eibl <friedrich.eibl@hu-berlin.de>
2024-12-07 15:36:29 +0100

    trying interface

--- conjutrain.py
+++ conjutrain.py
@@ -4,32 +4,15 @@ import random
 dict_file = "test_dict.csv"
 eval_file = "eval.csv"
 
-
-def write_csv(data, f_csv):
-    with open(f_csv, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerows(data)
-
 def train():
-    data = []
-    with open(dict_file, mode='r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            data.append(row)
-
-    learned = []
-    with open(eval_file, mode='r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            learned.append(row)
-
-    #print(data)
+    data = read_csv(dict_file)
+    learned = read_csv(eval_file)
 
     rounds = 0
     max_rounds = 30
     score = 0
-
-    pronouns = ["я", "ты", "он", "мы", "вы", "они"]
+    pronouns = get_pronouns()
+    
     while rounds < max_rounds:
         if not any('0' in word_learned for word_learned in learned):
             print('nothing left to learn !!!')
@@ -59,5 +42,20 @@ def train():
 
     print(f"Score:  {score}/{max_rounds}")
 
+def read_csv(file_path):
+	data = []
+	with open(file_path, mode='r') as file:
+		reader = csv.reader(file)
+		for row in reader:
+		    data.append(row)
+	return data
+
+def write_csv(data, f_csv):
+	with open(f_csv, mode='w', newline='') as file:
+		writer = csv.writer(file)
+		writer.writerows(data)
+		
+def get_pronouns():
+	return ["я", "ты", "он", "мы", "вы", "они"]
 
 train()
